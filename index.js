8c83cd9a12e69b330e95bae812e1d7bc969dd485
Luhlanga Loveness <124365198+LuhlangaLoveness@users.noreply.github.com>
2024-09-15 15:30:23 +0200

    added api key, api url and functions

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "5c293o80a8f158401teffb34fdeeebb7";
+  let apiurl =
+    "https://api.shecodes.io/weather/v1/current?query=#city&key=5c293o80a8f158401teffb34fdeeebb7&unit=merit";
+  //city
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement = addEventListener("submit", handleSearchSubmit);
