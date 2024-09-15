9719fb4ed3a90095b217f0a646b94ab28c66916b
Luhlanga Loveness <124365198+LuhlangaLoveness@users.noreply.github.com>
2024-09-15 16:06:04 +0200

    added search functionality using api

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+function refreshTempSize(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 function searchCity(city) {
   let apiKey = "5c293o80a8f158401teffb34fdeeebb7";
-  let apiurl =
-    "https://api.shecodes.io/weather/v1/current?query=#city&key=5c293o80a8f158401teffb34fdeeebb7&unit=merit";
-  //city
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=merit`;
+  axios.get(apiUrl).then(refreshTempSize);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
   searchCity(searchInputElement.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement = addEventListener("submit", handleSearchSubmit);
+searchCity("Nelspruit");
